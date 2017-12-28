cf0d3503ca375b7374e9e5d219c8cc3c0d0c94a0
Shelden Wilson <SheldenWilson@gmail.com>
2017-12-28 13:30:59 -0500

    added intent that links to google

--- app/src/main/java/com/example/swilson4264/imdb1/movieIT.java
+++ app/src/main/java/com/example/swilson4264/imdb1/movieIT.java
@@ -1,7 +1,10 @@
 package com.example.swilson4264.imdb1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class movieIT extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class movieIT extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_it);
+    }
+
+    public void addMovieScreen(View aMV){
+        Uri web = Uri.parse("http://www.imdb.com/title/tt1396484");
+        Intent intent = new Intent(Intent.ACTION_VIEW,web);
+
+        startActivity(intent);
     }
 }
